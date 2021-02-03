7c29f215d55ad9453e1e3295998ca9d643b55b5a
Eszkis <laszlo.szkis@gmail.com>
2021-02-03 12:20:40 +0100

    add function to convert a single line number to int

--- model/accountNumber/accountNumber.py
+++ model/accountNumber/accountNumber.py
@@ -2,11 +2,38 @@ from model.data_manager import read_table_from_file
 
 
 def separate_line_number_to_number_list(raw_data_lines):
-    numbers = []
-    for index in range(9):
-        number_lines = []
-        for line_count in range(3):
-            number_lines.append(raw_data_lines[line_count][0+index*3:3+index*3])
-        numbers.append(number_lines)
-    return numbers
+    account_numbers = []
+    for line_index in range(len(raw_data_lines) // 4):
+        numbers = []
+        for index in range(9):
+            number_lines = []
+            for line_count in range(1, 4):
+                number_lines.append(raw_data_lines[line_count + line_index * 4][0 + index * 3:3 + index * 3])
+            numbers.append(number_lines)
+        account_numbers.append(numbers)
+    return account_numbers
 
+
+def convert_line_number_to_integer(line_number):
+    if line_number[0] == '   ' and line_number[1] == '  |' and line_number[2] == '  |':
+        return 1
+    elif line_number[0] == ' _ ' and line_number[1] == ' _|' and line_number[2] == '|_ ':
+        return 2
+    elif line_number[0] == ' _ ' and line_number[1] == ' _|' and line_number[2] == ' _|':
+        return 3
+    elif line_number[0] == '   ' and line_number[1] == '|_|' and line_number[2] == '  |':
+        return 4
+    elif line_number[0] == ' _ ' and line_number[1] == '|_ ' and line_number[2] == ' _|':
+        return 5
+    elif line_number[0] == ' _ ' and line_number[1] == '|_ ' and line_number[2] == '|_|':
+        return 6
+    elif line_number[0] == ' _ ' and line_number[1] == '  |' and line_number[2] == '  |':
+        return 7
+    elif line_number[0] == ' _ ' and line_number[1] == '|_|' and line_number[2] == '|_|':
+        return 8
+    elif line_number[0] == ' _ ' and line_number[1] == '|_|' and line_number[2] == ' _|':
+        return 9
+    elif line_number[0] == ' _ ' and line_number[1] == '| |' and line_number[2] == '|_|':
+        return 0
+    else:
+        return "?"
