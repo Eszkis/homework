6e889fe62be7d19196621a21fb3da5d82ff7f20f
Eszkis <laszlo.szkis@gmail.com>
2021-02-04 12:04:14 +0100

    add userStory4Controller to show userstory4. moved relevant code to userStory4.py, and created uniq rawData

---
+++ controller/userStory4Controller.py
@@ -0,0 +1,10 @@
+from model import data_manager
+from model.accountNumber.userStory4 import convert_line_account_numbers_to_int_account_numbers
+
+
+def run():
+    input_file = "resource/input/rawData4.txt"
+    output_file = "resource/output/output4.txt"
+    lines = data_manager.read_table_from_file(input_file)
+    int_account_numbers = convert_line_account_numbers_to_int_account_numbers(lines)
+    data_manager.write_table_to_file(output_file, int_account_numbers)
