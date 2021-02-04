4da63fc968de1ba0f783058a19c96a890429e4a2
Eszkis <laszlo.szkis@gmail.com>
2021-02-04 11:57:01 +0100

    add menu for show all userStory

--- bankOCR.py
+++ bankOCR.py
@@ -1,11 +1,7 @@
 import os
-from model.data_manager import read_table_from_file
-from model.accountNumber import accountNumber
+from controller import main_controller
 
 if __name__ == '__main__':
     os.system('clear')
-    lines = read_table_from_file("model/accountNumber/rawData.txt")
-    int_account_numbers = accountNumber.convert_line_account_numbers_to_int_account_numbers(lines)
-    for number in int_account_numbers:
-        print(number)
+    main_controller.menu()
 
